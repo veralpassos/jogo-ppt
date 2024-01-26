35d8321791d036a6fc2ab19d39c14e4b72f2675c
verapassos <seu>
2024-01-26 00:37:30 -0300

    primeiro comit Jokenpô

---
+++ map.js
@@ -0,0 +1,12 @@
+/*
+Map => Mapear o nosso array.
+-Cria um novo array, a patir do array percorrrido (array original
+  )
+  - Cria um novo array, com a mesma quantidade de itens do array original
+  -item do array
+  -index
+  -array completo
+*/
+const number = [1, 2, 3, 4, 5, 6, 7];
+
+const double = number.map((item) => {});
